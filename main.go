4d70b21cf711c2ea32b12a81705de1a54c5e314b
Ifeoluwa Oyelakin <oyeife@gmail.com>
2022-10-16 20:25:12 +0100

    Pointers and user input

--- main.go
+++ main.go
@@ -11,4 +11,24 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticker here to attend")
 
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	 // ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v ticket(s). You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	
 }
